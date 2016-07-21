fd65d552d655c7fe5c611898f4ac369f4fc0dbcc
Adrian Gonzalez <adrigonle@gmail.com>
2016-07-21 12:52:47 +0200

    Added encoder to User

--- src/main/scala/models/User.scala
+++ src/main/scala/models/User.scala
@@ -1,9 +1,11 @@
 package models
 
 import java.time.LocalDateTime
+
 import com.twitter.finagle.exp.mysql._
 import com.twitter.util.Future
 import controllers.MySql
+import io.circe.{Encoder, Json}
 
 case class User(
                  id: Long,
@@ -20,10 +22,22 @@ object User extends MySql {
 
   val tableName = "user"
 
+  implicit val encoder = Encoder.instance[User] { u: User =>
+    Json.obj(
+      ("id", Json.fromLong(u.id)),
+      ("name", Json.fromString(u.name)),
+      ("surname", Json.fromString(u.surname)),
+      ("nif", Json.fromString(u.nif)),
+      ("email", Json.fromString(u.email)),
+      ("created_at", Json.fromString(u.createdAt.toString)),
+      ("updated_at", Json.fromString(u.updatedAt.toString))
+    )
+  }
+
   def find(userId: Long): Future[Option[User]] =
     client.prepare(
       s"""
-         |SELECT * FROM $tableName
+         |SELECT * FROM $tableName u
          |WHERE id = ?
        """.stripMargin)(userId)
     .map {
@@ -66,14 +80,14 @@ object User extends MySql {
     }
 
   def toEntity(row: Row): User = {
-    val LongValue(id) = row("u.id").get
-    val StringValue(name) = row("u.name").get
-    val StringValue(surname) = row("u.surname").get
-    val StringValue(nif) = row("u.nif").get
-    val StringValue(email) = row("u.email").get
-    val StringValue(digest) = row("u.digest").get
-    val TimestampValue(createdAt) = row("u.created_at").get
-    val TimestampValue(updatedAt) = row("u.updated_at").get
+    val LongValue(id) = row("id").get
+    val StringValue(name) = row("name").get
+    val StringValue(surname) = row("surname").get
+    val StringValue(nif) = row("nif").get
+    val StringValue(email) = row("email").get
+    val StringValue(digest) = row("digest").get
+    val TimestampValue(createdAt) = row("created_at").get
+    val TimestampValue(updatedAt) = row("updated_at").get
 
     User(
       id,
