6a78e64983a3ef30fa8ea6a91a598ba14b923c7c
Nicolas Vargas <nvortega92@gmail.com>
2016-07-31 17:51:37 +0200

    Added new route to find subjects by id, and updated Subject POJO

--- app/controllers/v1/SubjectController.java
+++ app/controllers/v1/SubjectController.java
@@ -2,11 +2,17 @@ package controllers.v1;
 
 import com.google.inject.Inject;
 import daos.SubjectDao;
+import models.Subject;
+import play.mvc.Result;
 
 public class SubjectController extends BaseController {
 
     @Inject
     private SubjectDao subjectDao;
 
+    public Result show(Integer id) {
+        Subject s = subjectDao.find(id);
 
+        return ok(gson.toJson(s));
+    }
 }
