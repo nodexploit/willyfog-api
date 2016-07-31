f6a1c21fba67918dfc23107f693495c503e45c6e
Nicolas Vargas <nvortega92@gmail.com>
2016-07-31 23:45:19 +0200

    Added route with some user info about degree, centre and his university

--- app/controllers/v1/UserController.java
+++ app/controllers/v1/UserController.java
@@ -5,6 +5,9 @@ import daos.UserDao;
 import models.User;
 import play.mvc.Result;
 
+import java.util.List;
+import java.util.Map;
+
 public class UserController extends BaseController {
 
     @Inject
@@ -14,5 +17,17 @@ public class UserController extends BaseController {
         User u = userDao.find(id);
 
         return ok(gson.toJson(u));
+    }
+
+    public Result userByToken(String accessToken) {
+        User u = userDao.find(accessToken);
+
+        return ok(gson.toJson(u));
+    }
+
+    public Result userInfo(Integer id) {
+        List<Map<String, Object>> result = userDao.getUserInfo(id);
+
+        return ok(gson.toJson(result));
     }
 }
--- app/daos/UserDao.java
+++ app/daos/UserDao.java
@@ -3,6 +3,9 @@ package daos;
 import models.User;
 import org.sql2o.Connection;
 
+import java.util.List;
+import java.util.Map;
+
 public class UserDao extends BaseDao {
 
     public static String tableName = "user";
@@ -36,5 +39,27 @@ public class UserDao extends BaseDao {
         }
 
         return user;
+    }
+
+    public List<Map<String, Object>> getUserInfo(Integer userId) {
+        String sql = "SELECT u.name, u.surname, u.nif, " +
+                "u.email, d.name AS degree_name, " +
+                "c.name AS centre_name, un.name AS university_name " +
+                "FROM " + tableName + " u " +
+                "JOIN user_enrolled_degree AS ued ON u.id = ued.user_id " +
+                "JOIN degree AS d ON ued.degree_id = d.id " +
+                "JOIN centre AS c ON d.centre_id = c.id " +
+                "JOIN university AS un ON c.university_id = un.id " +
+                "WHERE u.id = :userId";
+
+        List<Map<String, Object>> result = null;
+
+        try(Connection con = this.db.open()) {
+            result = toMapList(con.createQuery(sql)
+                    .addParameter("userId", userId)
+                    .executeAndFetchTable());
+        }
+
+        return result;
     }
 }
