4beaab2a517c027226f53a7637e20893dc73c460
Adrian Gonzalez <adrigonle@gmail.com>
2016-07-31 19:04:16 +0200

    Added request show call

--- app/controllers/v1/RequestController.java
+++ app/controllers/v1/RequestController.java
@@ -1,14 +1,9 @@
 package controllers.v1;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import com.google.inject.Inject;
 import daos.RequestDao;
 import daos.RequestDestinationSubjectDao;
-import models.Request;
-import models.RequestDestinationSubject;
 import play.mvc.Result;
-import scala.util.parsing.json.JSONObject;
 
 import java.util.List;
 import java.util.Map;
@@ -22,8 +17,8 @@ public class RequestController extends BaseController {
 
     public Result show(Integer id) {
         Map<String, Object> r = requestDao.find(id);
-//        List<RequestDestinationSubject> rds = requestDestinationDao.requestDestinations(id);
-//        r.put("destination_subject", rds);
+        List<Map<String, Object>> rds = requestDestinationDao.requestDestinations(id);
+        r.put("destination_subject", rds);
 
         return ok(gson.toJson(r));
     }
--- app/daos/RequestDestinationSubjectDao.java
+++ app/daos/RequestDestinationSubjectDao.java
@@ -1,6 +1,5 @@
 package daos;
 
-import models.RequestDestinationSubject;
 import org.sql2o.Connection;
 
 import java.util.List;
@@ -11,9 +10,16 @@ public class RequestDestinationSubjectDao extends BaseDao {
     public static String tableName = "request_destination_subject";
 
     public List<Map<String, Object>> requestDestinations(Integer requestId) {
-        String sql = "SELECT * " +
-                "FROM " + tableName + " " +
-                "WHERE request_id = :requestId";
+        String sql = "SELECT " +
+                "rds.subject_id, " +
+                "IFNULL(rds.subject_name, s.name) AS name, IFNULL(rds.subject_credits, s.credits) AS credits, " +
+                "IFNULL(rds.subject_code, s.code) AS code, " +
+                "IFNULL(rds.centre_name, c.name) AS centre_name, IFNULL(rds.centre_code, c.code) AS centre_code " +
+                "FROM request_destination_subject rds " +
+                "LEFT JOIN subject s ON rds.subject_id = s.id " +
+                "LEFT JOIN degree d ON s.degree_id = d.id " +
+                "LEFT JOIN centre c ON d.centre_id = c.id " +
+                "WHERE rds.request_id = :requestId";
 
         List<Map<String, Object>> requestDestinationSubjects;
         try(Connection con = this.db.open()) {
