1f759e449065984a4de2d1d95c52138a536616d2
Adrian Gonzalez <adrigonle@gmail.com>
2016-07-31 13:24:44 +0200

    Added env to application.conf

--- app/Module.java
+++ app/Module.java
@@ -1,11 +1,22 @@
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
 import org.sql2o.Sql2o;
+import play.Configuration;
 
 public class Module extends AbstractModule {
 
+    @Inject
+    private Configuration configuration;
+
     @Override
     protected void configure() {
+        String host = configuration.getString("database.host");
+        String dbName = configuration.getString("database.name");
+        String dbUsername = configuration.getString("database.username");
+        String dbPassword = configuration.getString("database.password");
+        String dsn = "jdbc:mysql://" + host + ":3306/" + dbName;
+
         bind(Sql2o.class)
-                .toInstance(new Sql2o("jdbc:mysql://localhost:3306/willyfog_db", "root", "root"));
+                .toInstance(new Sql2o(dsn, dbUsername, dbPassword));
     }
 }
