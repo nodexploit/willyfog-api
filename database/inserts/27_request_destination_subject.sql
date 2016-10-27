435ce153f5c0859e1527c655ac8564646afbbf64
Adrian Gonzalez <adrigonle@gmail.com>
2016-10-27 23:04:40 +0200

    Removed custom destination subject

--- database/inserts/27_request_destination_subject.sql
+++ database/inserts/27_request_destination_subject.sql
@@ -1,8 +1,4 @@
 INSERT INTO `willyfog_db`.`request_destination_subject` (request_id, subject_id) VALUES (1, 2);
 INSERT INTO `willyfog_db`.`request_destination_subject` (request_id, subject_id) VALUES (2, 3);
 INSERT INTO `willyfog_db`.`request_destination_subject` (request_id, subject_id) VALUES (2, 4);
-INSERT INTO `willyfog_db`.`request_destination_subject`
-(request_id, subject_name, subject_credits, subject_code, centre_name, city_name, university_name, degree_name)
-VALUES
-(3, 'Asignatura nueva', 4, '4fgh', 'Facultad nueva', 'Málaga', 'Universidad de Málaga', 'Grado de Química');
 INSERT INTO `willyfog_db`.`request_destination_subject` (request_id, subject_id) VALUES (3, 5);
